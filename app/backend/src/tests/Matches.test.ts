4e05d39ab91844c7d26ef708904aacb5c53d2f17
eduardoSousa <dudus.sousa4002@gmail.com>
2023-05-15 14:23:59 -0300

    teste de intengração da rota matches?inProgress

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -7,7 +7,11 @@ import { app } from '../app';
 import Matche from '../database/models/Matche.model';
 
 import { Response } from 'superagent';
-import MockReturnFindMatches from './mocks/Matches.mock';
+import {
+  MockReturnFindMatches,
+  MockReturnFindMatchesFalse,
+  MockReturnFindMatchestrue,
+} from './mocks/Matches.mock';
 
 chai.use(chaiHttp);
 
@@ -15,6 +19,9 @@ const { expect } = chai;
 
 describe('Verifica rotas Matches', () => {
   describe('Verifica requesições GET de Matches', () => {
+    afterEach(() => {
+      sinon.restore();
+    })
     it('Verifica se a requisição GET /matches retorna uma lista de partidas', async () => {
       sinon.stub(Matche, 'findAll').resolves(MockReturnFindMatches as [])
 
@@ -23,6 +30,24 @@ describe('Verifica rotas Matches', () => {
       
       expect(response.status).to.be.equal(200);
       expect(response.body).to.be.deep.equal(MockReturnFindMatches);
+    })
+    it('Verifica se ao fazer a requisição com "inProgress" TRUE, retorna somente partidas em andamento', async () => {
+      sinon.stub(Matche, 'findAll').resolves(MockReturnFindMatchestrue as [])
+
+      const response = await chai.request(app)
+        .get('/matches?inProgress=true');
+      
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal(MockReturnFindMatchestrue);
+    })
+    it('Verifica se ao fazer a requisição com "inProgress" FALSE, retorna somente partidas em andamento', async () => {
+      sinon.stub(Matche, 'findAll').resolves(MockReturnFindMatchesFalse as [])
+
+      const response = await chai.request(app)
+        .get('/matches?inProgress=false');
+      
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal(MockReturnFindMatchesFalse);
     })
   })
 });
--- app/backend/src/tests/mocks/Matches.mock.ts
+++ app/backend/src/tests/mocks/Matches.mock.ts
@@ -29,4 +29,71 @@ const MockReturnFindMatches = [
   }
 ]
 
-export default MockReturnFindMatches;
+const MockReturnFindMatchesFalse = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Grêmio"
+    }
+  },
+  {
+    id: 41,
+    homeTeamId: 16,
+    homeTeamGoals: 2,
+    awayTeamId: 9,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Internacional"
+    }
+  }
+];
+
+const MockReturnFindMatchestrue = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: true,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Grêmio"
+    }
+  },
+  {
+    id: 41,
+    homeTeamId: 16,
+    homeTeamGoals: 2,
+    awayTeamId: 9,
+    awayTeamGoals: 0,
+    inProgress: true,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Internacional"
+    }
+  }
+]
+
+
+export {
+  MockReturnFindMatches,
+  MockReturnFindMatchestrue,
+  MockReturnFindMatchesFalse,
+};
